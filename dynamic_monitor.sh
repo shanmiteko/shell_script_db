c3ebca269234c241c103166942e54fdd5ce3c463
shanmite <shanmite@hotmail.com>
2022-10-16 20:30:37 +0800

    update

---
+++ dynamic_monitor.sh
@@ -0,0 +1,13 @@
+#!/bin/env bash
+
+while true; do
+	update_num=$(curl -sL 'https://api.bilibili.com/x/polymer/web-dynamic/v1/feed/all/update?type=all&update_baseline=0' \
+		-H $'Cookie: SESSDATA=739c614e%2C1673441995%2Caf361*71;' |
+		jq '.data.update_num')
+
+	if [[ $update_num != "" ]] && [[ $update_num != "0" ]]; then
+		kdialog --title B站动态通知 --passivepopup "您有 $update_num 条未读动态" > /dev/null 2>&1
+	fi
+	echo "[$(date)] [B站动态通知] [您有 $update_num 条未读动态]"
+	sleep 300
+done
